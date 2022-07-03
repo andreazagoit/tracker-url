f9936cede4d294e9ceda3a374ba89639da0cdd01
Andrea Zago <info@andreazago.it>
2022-07-04 00:11:11 +0200

    feat: added db connection

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # tracker-url
-# tracker-url
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+  const { u: user, r: redirect } = req.query;
+  console.log(user, redirect);
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+mongoose
+  .connect(
+    "mongodb+srv://admin:bhCfkWLtVC5nC8BK@cluster0.bfr1oob.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to the DB");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => console.log("Error: ", error));
